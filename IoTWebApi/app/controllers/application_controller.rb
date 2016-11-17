9a1f719f16c9b04e6223d8826468dec6dfdf6474
Ferreira <pnlferreira@gmail.com>
2016-11-17 12:52:47 +0000

    Com autenticação por token.

--- IoTWebApi/app/controllers/application_controller.rb
+++ IoTWebApi/app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::API
+	#Para serializar todos os objectos
 	include ActionController::Serialization
+
+	#Para autenticar com token 
+	include ActionController::HttpAuthentication::Token::ControllerMethods
+
+	  # Add a before_action to authenticate all requests.
+	  # Move this to subclassed controllers if you only
+	  # want to authenticate certain methods.
+	  before_action :authenticate
+
+	  protected
+
+	  # Authenticate the user with token based authentication
+	  def authenticate
+	    authenticate_token || render_unauthorized
+	  end
+
+	  def authenticate_token
+	    authenticate_with_http_token do |token, options|
+	      @current_user = User.find_by(api_key: token)
+	    end
+	  end
+
+	  def render_unauthorized(realm = "Application")
+	    self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
+	    render json: 'Bad credentials', status: :unauthorized
+	  end
+
 end
--- IoTWebApi/db/schema.rb
+++ IoTWebApi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117120559) do
+ActiveRecord::Schema.define(version: 20161117124904) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20161117120559) do
     t.string   "addressDetails"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "api_key"
   end
 
 end
