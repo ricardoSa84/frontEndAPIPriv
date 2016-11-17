c8e875bb196acac74bdff10f8f793cdaa8061498
Ferreira <pnlferreira@gmail.com>
2016-11-17 12:47:11 +0000

    Configuração para Cross-Origin Resource Sharing (CORS) e para controlar o numero de pedidos num determinado intervalo de tempo.

--- IoTWebApi/config/application.rb
+++ IoTWebApi/config/application.rb
@@ -26,5 +26,17 @@ module IoTWebApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #CORS
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    #config for attacks
+    config.middleware.use Rack::Attack
+    
   end
 end
