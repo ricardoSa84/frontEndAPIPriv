a8ab7585843764c56067c2dc6b724296793ea246
Ricardo <ricardosa.mail@gmail.com>
2016-12-13 12:11:47 +0000

    schema

--- IoTWebApi/db/schema.rb
+++ IoTWebApi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210210120) do
+ActiveRecord::Schema.define(version: 20161212115825) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
